364396cdd7e24103eb0d2c1accef10be134aa8e7
Chantz-J <johnson.chantz@gmail.com>
2021-04-12 15:21:07 -0400

    Server up and running.

--- api/server.js
+++ api/server.js
@@ -1,1 +1,10 @@
+const express = require("express")
 
+const server = express()
+server.use(express.json())
+
+server.get("/", (req, res) => {
+    res.status(200).json(`Node db1 module project server is up and running!`)
+})
+
+module.exports = server
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const server = require("./api/server")
+const PORT = 8000
 
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
